46867a57eb19f879bbf1a40084a1dab0969ab504
JasonKTChen <49644422+JasonKTChen@users.noreply.github.com>
2022-03-13 17:58:11 -0700

    revert

--- lib/start.dart
+++ lib/start.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_slide_puzzle/carousel.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class StartPage extends StatefulWidget {
   const StartPage({Key? key}) : super(key: key);
